645af5292525fdc0fd0ab9367ba7a412fcbe2103
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-09-17 11:11:28 -0600

    Added HelloStudent component; uses props

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import HelloStudent from './components/HelloStudent';
 
 ReactDOM.render(
   <React.StrictMode>
-    <></>
+    <>
+    <HelloStudent name="React Application" />
+    <HelloStudent name="Redux" />
+    <HelloStudent name="React Router" />
+
+    </>
   </React.StrictMode>,
   document.getElementById('root')
 );
