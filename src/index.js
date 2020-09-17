709cdcbb2df4c29f98ad59a47b27a8fa9589d47b
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-09-17 10:52:05 -0600

    Added Helloworld component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import HelloWorld from './components/HelloWorld';
 
 ReactDOM.render(
   <React.StrictMode>
-    <></>
+    <h1> React GIT branch Merge Practice </h1>
+    <HelloWorld />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
